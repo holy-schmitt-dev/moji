8a831ba0c3d322ba20312c5996a0928c0ceedf64
Holy Schmitt Studios LLC <mike@holyschmitt.studio>
2026-01-19 20:55:14 -0600

    Initial Commit

---
+++ Moji/MojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojiApp.swift
+//  Moji
+//
+//  Created by Michael Schmitt on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
